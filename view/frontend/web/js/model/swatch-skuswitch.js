917bf8078c1459275d0b1a059d0d7dbe5e47de61
Alexander Menk <a.menk@imi.de>
2020-08-31 16:56:49 +0200

    Swatches: Port changed logic from skuswitch.js

--- view/frontend/web/js/model/swatch-skuswitch.js
+++ view/frontend/web/js/model/swatch-skuswitch.js
@@ -11,12 +11,20 @@ define([
 
         var updatePrice = targetModule.prototype._UpdatePrice;
         targetModule.prototype.dynamic = {};
+        targetModule.prototype.dynamicAttrs = [];
 
         $('[data-dynamic]').each(function(){
             var code = $(this).data('dynamic');
             var value = $(this).html();
+            var attrs = [];
+
+            // Get the initial attributes value to be able to reset them if a user selects an option and then selects none again
+            $.each(this.attributes, function() {
+                attrs[this.name] = this.value;
+            });
 
             targetModule.prototype.dynamic[code] = value;
+            targetModule.prototype.dynamicAttrs[code] = attrs;
         });
 
         var updatePriceWrapper = wrapper.wrap(updatePrice, function(original){
@@ -25,6 +33,7 @@ define([
             for (var code in dynamic){
                 if (dynamic.hasOwnProperty(code)) {
                     var value = "";
+                    var attrs = [];
                     var $placeholder = $('[data-dynamic='+code+']');
                     var allSelected = true;
 
@@ -41,11 +50,20 @@ define([
                     if(allSelected){
                         var products = this._CalcProducts();
                         value = this.options.jsonConfig.dynamic[code][products.slice().shift()].value;
+                        attrs = this.options.jsonConfig.dynamic[code][products.slice().shift()].attrs;
                     } else {
                         value = this.dynamic[code];
+                        attrs = this.dynamicAttrs[code];
                     }
 
                     $placeholder.html(value);
+
+                    // Set all attributes if we have some
+                    if(attrs != undefined) {
+                        for(var a in attrs) {
+                            $placeholder.attr(a, attrs[a]);
+                        }
+                    }
                 }
             }
 
